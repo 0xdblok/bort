4e149e10a441ff64f4289a060df7153b695badcd
0xdblok <dblok0x@gmail.com>
2024-07-08 13:53:05 +0200

    ee

--- app/components/tokenomics.js
+++ app/components/tokenomics.js
@@ -2,24 +2,26 @@ import React from "react";
 
 export default function Tokenomics() {
   return (
-    <div className="text-center animate__animated animate__fadeInUp pb-8  pt-6">
-      <p className="lg:text-6xl animate__animated animate__fadeInUp md:text-4xl [text-shadow:2px_2px_0px_rgba(46,56,125)]   text-yellow-400 text-center text-3xl  pt-16">
-        BORTNOMICS
-      </p>
-      <div className="pt-3 ext-yellow-400 text-[#161709]">
-        <p className="tracking-[.08em] animate__animated animate__fadeInUp">
-          TOTAL SUPPLY
+    <div className="text-center animate__animated animate__fadeInUp pb-8  pt-6 ">
+      <div className="bg-[#087995] mx-24 rounded-md pb-10">
+        <p className="lg:text-6xl animate__animated animate__fadeInUp md:text-4xl [text-shadow:2px_2px_0px_rgba(46,56,125)]   text-yellow-400 text-center text-3xl  pt-16">
+          BORTNOMICS
         </p>
-        <p className=" tracking-[.12em] animate__animated animate__fadeInUp">
-          1 BILLION
-        </p>
-        <p className=" tracking-[.08em] animate__animated animate__fadeInUp">
-          fair launch for all BORT out there!!!{" "}
-        </p>
-      </div>
-      <div className="pt-10  text-center text-[#161709]">
-        <p className=" tracking-[.08em] font-bold">Contract Address</p>
-        <p className=" tracking-[.08em]">nbvnvbbvnvbnbvnbvn</p>
+        <div className="pt-3 ext-yellow-400 text-[#161709]">
+          <p className="tracking-[.08em] animate__animated animate__fadeInUp font-semibold text-white">
+            TOTAL SUPPLY
+          </p>
+          <p className=" tracking-[.12em] animate__animated animate__fadeInUp text-white font-semibold">
+            1 BILLION
+          </p>
+          <p className=" tracking-[.08em] animate__animated animate__fadeInUp text-white pt-2 ">
+            fair launch for all BORT out there!!!{" "}
+          </p>
+        </div>
+        <div className="pt-10  text-center   text-white">
+          <p className=" tracking-[.08em] font-bold">Contract Address</p>
+          <p className=" tracking-[.08em]">TBA</p>
+        </div>
       </div>
     </div>
   );
